611639016e4953dda6849e356dee34fe52d5923d
Marco Braga <maboa17@gmail.com>
2026-02-13 21:05:31 -0300

    feat: inicializar projeto Spring Boot e configurar dependências

---
+++ src/main/java/com/mv/appointment/MedicalAppointmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.mv.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mv/appointment/MedicalAppointmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
